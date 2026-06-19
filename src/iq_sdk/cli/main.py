9374f7942ff5798c610671444a1713a716dfb8bb
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-06-19 15:34:55 -0400

    Add verbose option

--- src/iq_sdk/cli/main.py
+++ src/iq_sdk/cli/main.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 import pathlib
+from typing import Annotated
 from tyro.extras import SubcommandApp
 import xxhash
 from rich.console import Console
 import yaml
+import tyro
 
 app = SubcommandApp()
 console = Console()
 
 
-def check_file(file: pathlib.Path, crc: str | None, seed: int) -> bool:
+def check_file(file: pathlib.Path, crc: str | None, seed: int, verbose: bool) -> bool:
     if not file.exists():
         if crc is not None:
             console.print(f"{file.name}: [red]Does not exist[/red]")
@@ -19,20 +21,25 @@ def check_file(file: pathlib.Path, crc: str | None, seed: int) -> bool:
         buffer = f.read()
     calculated = xxhash.xxh64(buffer, seed).hexdigest()
     ret = calculated == crc
-    if ret:
+    if ret and verbose:
         console.print(f"{file.name}: [green]OK[/green]")
-    else:
+    elif not ret:
         console.print(f"{file.name}: [red]Failed ({calculated} != {crc})[/red]")
     return ret
 
 
 @app.command
-def verify(trace: pathlib.Path, /) -> None:
+def verify(trace: pathlib.Path, /,
+           verbose: Annotated[tyro.conf.UseCounterAction[int], tyro.conf.arg(aliases=["-v"])] = 0) -> None:
     """Verify the files in a capture run.
 
     Args:
         trace: Path to the trace directory to verify. Must contain a checksum.yaml file.
+        verbose: Verbose mode
     """
+
+    verbose = verbose > 0
+
     if not trace.exists():
         console.print(f"{trace} does not exist")
         exit(1)
@@ -52,7 +59,7 @@ def verify(trace: pathlib.Path, /) -> None:
 
     ok = True
     for file, crc in checksums.items():
-        ok = check_file(trace / file, crc, seed)
+        ok = check_file(trace / file, crc, seed, verbose)
 
     if not ok:
         console.print("[red]One or more files in the trace has data integrity issues[/red]")
